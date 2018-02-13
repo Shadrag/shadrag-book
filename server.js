efb4f79aee6b0e045cd8b0c1c36cc5f4c26bfa97
Anton Buslaev <antonbuslaev@gmail.com>
2018-02-13 22:12:09 +0300

    Замена библиотеки api и тестовый бот

--- server.js
+++ server.js
@@ -1,51 +1,16 @@
-var express = require('express');
-var app = express();
+const Telegraf = require('telegraf');
 
-const TelegramBot = require('node-telegram-bot-api');
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const token = process.env.BOT_TOKEN || '***';
+bot.start((ctx) => {
+    console.log('started:', ctx.from.id);
+    return ctx.reply('Welcome!')
+})
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+bot.command('help', (ctx) => ctx.reply('Try send a sticker!'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there!'));
+bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
 
+bot.startPolling();
 
-// const server = app.listen(port, function () {
-//     console.log('listen', port);
-// });
-
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function (request, response) {
-    response.send('Hello World!');
-});
-
-app.listen(app.get('port'), function () {
-    console.log("Node app is running at localhost:" + app.get('port'));
-});
-
-
-
-
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-
-// send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-
-// send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
-});
