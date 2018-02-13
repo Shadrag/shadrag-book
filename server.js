1d5791299ca185eed7f30944bd0861aecb11c257
Shadrag <antonbuslaev@gmail.com>
2018-02-13 14:02:31 +0300

    Добавлен сервер

--- server.js
+++ server.js
@@ -9,19 +9,21 @@ const token = process.env.BOT_TOKEN || '***';
 const bot = new TelegramBot(token, {polling: true});
 
 
+// const server = app.listen(port, function () {
+//     console.log('listen', port);
+// });
 
-// app.set('port', (process.env.PORT || 5000));
-// app.use(express.static(__dirname + '/public'));
-//
-//
-// app.get('/', function (request, response) {
-//     response.send('Hello World!');
-// });
-//
-// app.listen(app.get('port'), function () {
-//     console.log("Node app is running at localhost:" + app.get('port'));
-// });
-//
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (request, response) {
+    response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
+
 
 
 
